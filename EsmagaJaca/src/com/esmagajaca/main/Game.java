0980dde9e1b60de4548784c9c17f95f42485506f
juliasgomes <67568246+juliasgomes@users.noreply.github.com>
2022-07-22 14:41:09 -0300

    Spritesheet, Player and Entity

--- EsmagaJaca/src/com/esmagajaca/main/Game.java
+++ EsmagaJaca/src/com/esmagajaca/main/Game.java
@@ -1,19 +1,49 @@
 package com.esmagajaca.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
+import com.esmagajaca.entities.Entity;
+import com.esmagajaca.entities.Player;
+import com.esmagajaca.graficos.Spritesheet;
+
 public class Game extends Canvas implements Runnable{
 
+	
+	private static final long serialVersionUID = 1L;
 	public static JFrame frame;
-	private final int WIDTH = 160;
-	private final int HEIGHT = 120;
-	private final int SCALE = 4;
+	private Thread thread;
+	private boolean isRunning;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+	
+	private BufferedImage image;
+	
+	public List<Entity> entities;
+	public Spritesheet personagens;
 	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		//Inicializando objetos
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		entities = new ArrayList<Entity>();
+		personagens = new Spritesheet("/personagens.png");
+		
+		Player player = new Player(0,0,16,16,personagens.getSprite(0, 0, 32, 32));
+		entities.add(player);
+	}
+	
+	public void initFrame() {
 		frame = new JFrame("Esmaga Jaca Simulator");
 		frame.add(this);
 		frame.setResizable(false);
@@ -23,11 +53,81 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String args[]) {
 		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick() {
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(0,0,0));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		// Renderização
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+		bs.show();
 	}
 	
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
 		
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta+= (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer+= 1000;
+			}
+		}
+		
+		stop();
 	}
 }
